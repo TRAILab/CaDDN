d94706e8da697a7e48f7c9ccd95bac758561bfe4
codyreading <codyreading@gmail.com>
2021-03-01 18:42:13 -0500

    Removed spconv refs and added CaDDN config

--- pcdet/models/detectors/__init__.py
+++ pcdet/models/detectors/__init__.py
@@ -1,5 +1,4 @@
 from .detector3d_template import Detector3DTemplate
-from .PartA2_net import PartA2Net
 from .point_rcnn import PointRCNN
 from .pointpillar import PointPillar
 from .pv_rcnn import PVRCNN
@@ -8,7 +7,6 @@ from .second_net import SECONDNet
 __all__ = {
     'Detector3DTemplate': Detector3DTemplate,
     'SECONDNet': SECONDNet,
-    'PartA2Net': PartA2Net,
     'PVRCNN': PVRCNN,
     'PointPillar': PointPillar,
     'PointRCNN': PointRCNN
--- pcdet/models/roi_heads/__init__.py
+++ pcdet/models/roi_heads/__init__.py
@@ -1,11 +1,9 @@
-from .partA2_head import PartA2FCHead
 from .pointrcnn_head import PointRCNNHead
 from .pvrcnn_head import PVRCNNHead
 from .roi_head_template import RoIHeadTemplate
 
 __all__ = {
     'RoIHeadTemplate': RoIHeadTemplate,
-    'PartA2FCHead': PartA2FCHead,
     'PVRCNNHead': PVRCNNHead,
     'PointRCNNHead': PointRCNNHead
 }
