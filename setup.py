1bdd243f22267f76439d125c8fa32f92fd05439b
codyreading <codyreading@gmail.com>
2021-03-01 18:23:01 -0500

    Removed LiDAR preprocessing

--- setup.py
+++ setup.py
@@ -28,24 +28,23 @@ def write_version_to_file(version, target_file):
 
 
 if __name__ == '__main__':
-    version = '0.3.0+%s' % get_git_commit_number()
+    version = '0.1.0+%s' % get_git_commit_number()
     write_version_to_file(version, 'pcdet/version.py')
 
     setup(
         name='pcdet',
         version=version,
-        description='OpenPCDet is a general codebase for 3D object detection from point cloud',
+        description='CaDDN is a monocular 3D object detection method.',
         install_requires=[
             'numpy',
             'torch>=1.1',
-            'spconv',
             'numba',
             'tensorboardX',
             'easydict',
             'pyyaml'
         ],
-        author='Shaoshuai Shi',
-        author_email='shaoshuaics@gmail.com',
+        author='Cody Reading',
+        author_email='codyreading@gmail.com',
         license='Apache License 2.0',
         packages=find_packages(exclude=['tools', 'data', 'output']),
         cmdclass={'build_ext': BuildExtension},
@@ -75,37 +74,6 @@ if __name__ == '__main__':
                     'src/roipoint_pool3d.cpp',
                     'src/roipoint_pool3d_kernel.cu',
                 ]
-            ),
-            make_cuda_ext(
-                name='pointnet2_stack_cuda',
-                module='pcdet.ops.pointnet2.pointnet2_stack',
-                sources=[
-                    'src/pointnet2_api.cpp',
-                    'src/ball_query.cpp',
-                    'src/ball_query_gpu.cu',
-                    'src/group_points.cpp',
-                    'src/group_points_gpu.cu',
-                    'src/sampling.cpp',
-                    'src/sampling_gpu.cu', 
-                    'src/interpolate.cpp', 
-                    'src/interpolate_gpu.cu',
-                ],
-            ),
-            make_cuda_ext(
-                name='pointnet2_batch_cuda',
-                module='pcdet.ops.pointnet2.pointnet2_batch',
-                sources=[
-                    'src/pointnet2_api.cpp',
-                    'src/ball_query.cpp',
-                    'src/ball_query_gpu.cu',
-                    'src/group_points.cpp',
-                    'src/group_points_gpu.cu',
-                    'src/interpolate.cpp',
-                    'src/interpolate_gpu.cu',
-                    'src/sampling.cpp',
-                    'src/sampling_gpu.cu',
-
-                ],
             ),
         ],
     )
